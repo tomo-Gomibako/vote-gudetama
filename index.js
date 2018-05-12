5921a64044bb86e035c53c44cec5d32774fd44a6
tomo <tomo.mouse.9086@gmail.com>
2018-05-12 17:03:56 +0900

    reset

--- index.js
+++ index.js
@@ -1,45 +1,59 @@
 const puppeteer = require("puppeteer")
 const promisify = require("util").promisify
-const fs = require("fs")
-const stat = promisify(fs.stat)
-const mkdir = promisify(fs.mkdir)
-const request = require("request")
+// const fs = require("fs")
+// const stat = promisify(fs.stat)
+// const mkdir = promisify(fs.mkdir)
 
-const vote = async () => {
-	const browser = await puppeteer.launch()
-	const page = await browser.newPage()
-	await page.setViewport({
-		width: 1200,
-		height: 1800
-	})
-	await page.goto("https://sanriocharacterranking.com/characters/gudetama/?=vote")
-	try {
-		await stat("screenshots/")
-	} catch(err) {
-		await mkdir("screenshots/")
+const clearCookies = async (page) => {
+	const cookies = await page.cookies()
+	for(let i in cookies) {
+		await page.deleteCookie(cookies[i])
+		// console.log(cookies[i])
 	}
-	await page.screenshot({ path: "screenshots/1.png" })
+	// console.log(await page.cookies())
+}
+const vote = async (browser, character = 0) => {
+	const page = await browser.newPage()
+	// await page.setViewport({
+	// 	width: 1200,
+	// 	height: 1800
+	// })
+	const characters = ["https://sanriocharacterranking.com/characters/gudetama/?=vote", "https://sanriocharacterranking.com/characters/kirimichan/?=vote"]
+	if(character >= characters.length) character = 0
+	await page.goto(characters[character])
+	await page.waitFor(1000)
+	// try {
+	// 	await stat("screenshots/")
+	// } catch(err) {
+	// 	await mkdir("screenshots/")
+	// }
+	// await page.screenshot({ path: "screenshots/1.png" })
 	await page.click("#js-modalConfirm_vote")
 	// await page.waitForNavigation("networkidle0")
-	await page.screenshot({ path: "screenshots/2.png" })
+	// await page.screenshot({ path: "screenshots/2.png" })
 	const age = (Math.random() * 15 | 0) + 5
 	const gender = 1
 	const region = (Math.random() * 47 | 0) + 1
 	console.log(age, gender, region)
+	await page.waitFor(1500)
 	await page.select("#userSelect-age", "" + age)
 	await page.select("#userSelect-gender", "" + gender)
 	await page.select("#userSelect-area", "" + region)
 	await page.click(".mainContent_form_agree_checkLabel").catch(e => { console.log(e) })
-	await page.screenshot({ path: "screenshots/3.png" })
-	await page.waitFor(2000)
+	// await page.screenshot({ path: "screenshots/3.png" })
+	await page.waitFor(1500)
 	await page.click("#js-btnVote")
-	await page.screenshot({ path: "screenshots/4.png" })
-	await page.waitFor(2000)
+	// await page.screenshot({ path: "screenshots/4.png" })
+	await page.waitFor(1500)
 	await page.click("#js-modalConfirm_vote")
-	await page.waitFor(2000)
-	await page.screenshot({ path: "screenshots/5.png" })
+	await page.waitFor(2500)
+	// await page.screenshot({ path: "screenshots/5.png" })
 	// await page.waitForNavigation("networkidle0")
-	await browser.close()
+	await clearCookies(page)
+	await page.evaluate(() => {
+	    localStorage.clear()
+	})
+	await page.close()
 }
 const wait = (ms) => {
 	return new Promise((resolve, reject) => {
@@ -52,31 +66,20 @@ const wait = (ms) => {
 		}
 	})
 }
-const post = async (text) => {
-	request.post({
-		url: "https://slack.com/api/chat.postMessage",
-		headers: {
-			"content-type": "application/json"
-		},
-		body: JSON.stringify({
-			token: "xoxp-309436344468-309454235106-363732822951-3a9e5ad38d99a2fda8a5470f0b0ec39d",
-			channel: "D92PM6KU0",
-			text: `${ text }`
-		})
-	})
-}
 const main = async () => {
+	const browser = await puppeteer.launch({ headless: false })
 	const loop = 2000
 	for(let i = 0; i < loop; i++) {
-		await vote().catch(err => {
+		await vote(browser, i % 10).catch(err => {
 			// await post(err)
 		})
-		console.log(`投票 ${ i + 1 }回目`)
-		if(i % 50 === 0) {
-			// await post(`投票 ${ i + 1 }回目`)
-		}
-		if(i !== loop - 1) await wait(1000)
+		console.log(`投票 ${ i + 1 }回`)
+		// if(i % 50 === 0) {
+		// 	await post(`投票 ${ i + 1 }回目`)
+		// }
+		// if(i !== loop - 1) await wait(1000)
 	}
+	browser.close()
 }
 
 main()
